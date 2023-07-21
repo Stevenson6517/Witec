52afcec1d8de36a5da809f248ea7110a2d07cc2c
David Curie <david.a.curie@vanderbilt.edu>
2023-07-21 15:06:53 -0500

    Stash WIP; make notebook viewer use utils
    Saving progress on a refactoring effort. These changes probably deserve
    to be part of their own branch, but because they depend on specific code
    being developed in this filenaming branch, I am testing the changes in
    this branch.

--- notebooks/spe_viewer.py
+++ notebooks/spe_viewer.py
@@ -7,297 +7,71 @@
 # - View a series of spectra linked in one file
 
 # +
-from collections import deque
-from collections.abc import Set, Mapping
-from dateutil import parser
-from glob import glob
+from pathlib import Path
 import matplotlib.pyplot as plt
 from mpl_toolkits import axes_grid1
 from numbers import Number
 import numpy as np
-import os
-import re
+
+import subprocess
 import sys
+import os
+import matplotlib.pyplot as plt
+from warnings import warn
+import h5py
+import pyUSID as usid
+import sidpy as sid
+import SciFiReaders as sr
 
-import winspec
+# from IPython.display import clear_output
+# # %matplotlib inline
 
-class SPE:
-    def __init__(self, file):
-        self.spe_dict = winspec.read_spe(file)
-        self.restore_datatypes()
-        self.extract_metadata()
-        self.build_spectroscopic_dataset()
-
-    def __repr__(self):
-        description = {
-            'Filename' : self.SPEFNAME,
-            'Observation date' : self.OBSDATE,
-            'Data shape' : self.spe_dict['data'].shape,
-            'Accumulations' : self.ACCUMULATIONS,
-            'Exposure (sec)' : self.EXPOSURE,
-            'Background corrected' : self.BACKGROUND,
-            'Flatfield corrected' : self.FLATFIELD,
-            'Center wavelength (nm)' : self.XCALIB['SpecCenterWlNm'],
-            'Grating (g/mm)' : self.XCALIB['SpecGrooves'],
-            'Chip Temp (C)' : self.CHIPTEMP,
-            'Size in memory (kb)' : '{:,d}'.format(round(self.getsize()/1e3))
-        }
-        return '\n'.join([f'{key:<22}: {value}' for key, value in description.items()])
-
-    def restore_datatypes(self):
-        """Reduce spe_dict filesize by converting data to uint16."""
-        self.spe_dict['data'] = np.asarray(self.spe_dict['data'], dtype=np.int32)
-        return None
-
-    def extract_metadata(self):
-        self.IGAIN = self.spe_dict['IGAIN']
-        self.EXPOSURE = self.spe_dict['EXPOSURE']
-        self.SPEFNAME = self.spe_dict['SPEFNAME']
-        self.OBSDATE = parser.parse(self.spe_dict['OBSDATE'].decode('UTF-8')).date()
-        self.CHIPTEMP = self.spe_dict['CHIPTEMP']
-        self.COMMENTS = self.spe_dict['COMMENTS'].decode('UTF-8')
-        self.XCALIB = self.spe_dict['XCALIB']
-        self.ACCUMULATIONS = self.spe_dict['ACCUMULATIONS']
-        self.FLATFIELD = self.spe_dict['FLATFIELD']
-        self.BACKGROUND = self.spe_dict['BACKGROUND']
-        # Helpful variables
-        self.slug = self.strip_extension(self.SPEFNAME)
-        self.basename = os.path.basename(self.slug)
-        return None
-    
-    def bytes_to_string(self, array, encoding='utf-8'):
-        string = []
-        for byte in array:
-            string.append(byte.decode(encoding))
-        return ''.join(string)
-
-    def extract_main_dataset(self):
-        """Extract a 2D array of acquisitions of shape (n, spectrum).
-        
-        Bins all pixels in a vertical column and sums the intensity in each bin.
-        Each spectrum is a 1D array that is the same width as the ROI during acquisition.
-        """
-        return np.sum(self.spe_dict['data'], axis=1, dtype=np.int32)
-
-    def build_spectroscopic_dataset(self, calibration='poly'):
-        """Map calibrated wavelength to the CCD pixels in ROI."""
-        columns = int(self.XCALIB['pixel_position'][2])
-        self.spectroscopic_indices = np.linspace(1, columns, columns, dtype=np.uint16)
-        if calibration == 'poly':
-            calibrate = np.poly1d(np.array(self.XCALIB['polynom_coeff'][2::-1]))
-            self.spectroscopic_values = calibrate(self.spectroscopic_indices)
-        elif calibration == 'linear':
-            start_wl = self.XCALIB['calib_value'][0]
-            end_wl = self.XCALIB['calib_value'][2]
-            self.spectroscopic_values = np.linspace(start_wl, end_wl, columns)
-        label = self.XCALIB['string']
-        label = self.bytes_to_string(label).strip('x\00')
-        label = label.replace('[', '(').replace(']', ')')
-        self.spectroscopic_label = label
-        self.spectroscopic_units = label.split('(')[1].split(')')[0]
-        return None
-
-    def extract_positions_from_file(self, filename):
-        """Extract relevant data from an accompanying file to be fed into position dataset.
-        
-        Files may be an exported .dat file or a .WIP file.
-        """
-        search_pattern = {'PointsPerLine' : 'Points',
-                          'LinesPerImage' : 'Lines',
-                          'ScanWidth' : 'Width',
-                          'ScanHeight' : 'Height',
-                          'ScanUnit' : 'Unit',
-                         }
-        match = dict.fromkeys(search_pattern,)
-        with open(filename, 'r', encoding='latin-1') as f:
-            header = f.readlines()[1:70]
-            for key, pattern in search_pattern.items():
-                for line in header:
-                    if re.search(pattern, line):
-                        try:
-                            # Normal match will result in string of numbers
-                            value = line.split()[-1]
-                            assert len(value) < 4
-                        except AssertionError:
-                            # Handles case where ScanUnit is buried in binary string
-                            refined_pattern = r'StandardUnit(.{1,})UnitKind'
-                            # Match everything between the words above
-                            binary_match = re.search(refined_pattern, line).group(1)
-                            value = binary_match[-6:-4] # pick out bits that correspond to unit
-                        match[key] = value
-                        break
-        return match.values() # PointsPerLine, LinesPerImage, ScanWidth, ScanHeight, ScanUnit
-
-    def read_positions_from_arguments(self, PointsPerLine, LinesPerImage, ScanWidth=None, ScanHeight=None, ScanUnit=None):
-        """Read user specified dimensions and supply default values for nonspecified parameters"""
-        ScanWidth = ScanWidth if ScanWidth else PointsPerLine
-        ScanHeight = ScanHeight if ScanHeight else LinesPerImage
-        ScanUnit = ScanUnit if ScanUnit else 'index'
-        return PointsPerLine, LinesPerImage, ScanWidth, ScanHeight, ScanUnit
-
-    def prompt_for_positions(self):
-        """Ask user to supply image dimensions; supply default values for nonspecified parameters"""
-        x = input('Points per Line:')
-        y = input('Lines per Image:')
-        return x, y, x, y, 'index'
-
-    def gather_position_data(self, supplemental=None, PointsPerLine=None,
-                             LinesPerImage=None, ScanWidth=None, ScanHeight=None, ScanUnit=None):
-        """Search for position data from supplemental file or arguments. Prompt otherwise."""
-        if supplemental is not None:
-            params = self.extract_positions_from_file(supplemental)
-        elif PointsPerLine is not None and LinesPerImage is not None:
-            params = self.read_positions_from_arguments(PointsPerLine, LinesPerImage,
-                                                        ScanWidth=ScanWidth, ScanHeight=ScanHeight,
-                                                        ScanUnit=ScanUnit)
-        else:
-            params = self.prompt_for_positions()
-        return params
-
-    def build_position_dataset(self, PointsPerLine, LinesPerImage, ScanWidth, ScanHeight, ScanUnit):
-        """Correlate the acquisition number with a physical position coordinate."""
-        # The dimensions of image (x,y) acquired in a spectral image
-        self.x_pixels = int(PointsPerLine)
-        self.y_pixels = int(LinesPerImage)
-        step_x = float(ScanWidth) / self.x_pixels 
-        step_y = float(ScanHeight) / self.y_pixels 
-        self.position_units = ScanUnit
-        
-        position_indices = []
-        for y in range(self.y_pixels):
-            for x in range(self.x_pixels):
-                position_indices.append((x,y))
-        
-        self.position_indices = np.asarray(position_indices, dtype=np.uint16)
-        self.position_values = self.position_indices * (step_x, step_y)
-        return None
-
-    def link_supplemental(self, file):
-        """Incorporate a supplemental data file that contains position data."""
-        params = self.extract_positions_from_file(file)
-        self.supplemental = file
-        self.build_position_dataset(*params)
-        return None
-
-    def strip_extension(self, filename):
-        return os.path.splitext((filename))[0]
-
-    def getsize(self):
-        """Recursively iterate to sum size of object & members."""
-        ZERO_DEPTH_BASES = (str, bytes, Number, range, bytearray)
-        _seen_ids = set()
-        def inner(obj):
-            obj_id = id(obj)
-            if obj_id in _seen_ids:
-                return 0
-            _seen_ids.add(obj_id)
-            size = sys.getsizeof(obj)
-            if isinstance(obj, ZERO_DEPTH_BASES):
-                pass # bypass remaining control flow and return
-            elif isinstance(obj, (tuple, list, Set, deque)):
-                size += sum(inner(i) for i in obj)
-            elif isinstance(obj, Mapping) or hasattr(obj, 'items'):
-                size += sum(inner(k) + inner(v) for k, v in getattr(obj, 'items')())
-            # Check for custom object instances - may subclass above too
-            if hasattr(obj, '__dict__'):
-                size += inner(vars(obj))
-            if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
-                size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
-            return size
-        return inner(self)
-
-    def add_colorbar(self, im, aspect=20, pad_fraction=0.5, **kwargs): # See https://nbviewer.jupyter.org/github/mgeier/python-audio/blob/master/plotting/matplotlib-colorbar.ipynb
-        """Add a properly scaled colorbar to a plot image."""
-        divider = axes_grid1.make_axes_locatable(im.axes)
-        width = axes_grid1.axes_size.AxesY(im.axes, aspect=1./aspect)
-        pad = axes_grid1.axes_size.Fraction(pad_fraction, width)
-        current_ax = plt.gca()
-        cax = divider.append_axes("right", size=width, pad=pad)
-        plt.sca(current_ax)
-        return im.axes.figure.colorbar(im, cax=cax, **kwargs)
-
-    def find_nearest(self, array, value):
-        """Return the index in array that is closest to value."""
-        array = np.asarray(array)
-        idx = (np.abs(array - value)).argmin()
-        return idx
-    
-    def index_from_coord(self, real_coord):
-        """Return the index corresponding to a specified coordinate from the heatmap.
-        
-        Parameters
-        ----------
-        real_coord : (x, y) tuple
-            The coordinates corresponding to the location in the scaled image. Top left is (0, 0).
-        
-        Returns
-        -------
-        index : int
-            The matching index of the main_dataset corresponding to the real coordinate.
-        """
-        condition = np.logical_and(self.position_values[:, 0] == real_coord[0],
-                                   self.position_values[:, 1] == real_coord[1])
-        return np.argwhere(condition)[0][0]
-        
-    
-    def spectrum(self, index):
-        """Return a single spectrum from the main dataset."""
-        spectra = self.extract_main_dataset()
-        return spectra[index]
-    
-    def plot_spectrum(self, ax, index, **kwargs):
-        spectrum = ax.plot(self.spectroscopic_values, self.spectrum(index), **kwargs)
-        ax.set_xlabel(self.spectroscopic_label)
-        ax.set_ylabel('Intensity')
-        return spectrum
-
-    def heatmap(self, ax, supplemental=None, PointsPerLine=None, LinesPerImage=None, ScanWidth=None,
-                ScanHeight=None, ScanUnit=None, start_wl=None, end_wl=None, **kwargs):
-        # Specify default behavior
-        if not start_wl:
-            start_wl = round(self.spectroscopic_values[0])
-        if not end_wl:
-            end_wl = round(self.spectroscopic_values[-1])
-        if not hasattr(self, 'position_indices'):
-            # Position dataset does not already exist. Search arguments or prompt for values
-            print("Missing additional position data.\n")
-            params = self.gather_position_data(supplemental=supplemental,
-                                               PointsPerLine=PointsPerLine, LinesPerImage=LinesPerImage,
-                                               ScanWidth=ScanWidth, ScanHeight=ScanHeight,
-                                               ScanUnit=ScanUnit)
-            self.build_position_dataset(*params)
-        
-        # Calculate supplemental plotting parameters
-        x = self.position_values[:, 0]
-        y = self.position_values[:, 1]
-        extents = (x[0], x[-1], y[-1], y[0])  # left, right, bottom, top
-        start = self.find_nearest(self.spectroscopic_values, start_wl)
-        end = self.find_nearest(self.spectroscopic_values, end_wl)
-        
-        # Format data to plot
-        data = self.extract_main_dataset()
-        integrated_counts = np.sum(data[:, start:end], axis=1)
-        integrated_counts_2D = integrated_counts.reshape(self.y_pixels, self.x_pixels)
-        
-        # Generate and format plot
-        heatmap = ax.imshow(integrated_counts_2D, extent=extents, **kwargs)
-        self.add_colorbar(heatmap)
-        ax.set_xlabel(f'Position ({self.position_units})')
-        ax.set_ylabel(f'Position ({self.position_units})')
-        ax.set_title(f'{self.basename}\n({start_wl}\u2013{end_wl} nm)')
-        return heatmap
+import witec.utils
+from witec.spe import SPE
+from witec.winspec import SpeFile
+from witec.project import Witec
 # -
 
-# files = sorted(glob('Ryan/Si-Pillars/*.SPE'))
-files = sorted(glob('Ryan/WashU/*.SPE'))
-for i, file in enumerate(files):
-    print(i, file)
+data_path = r'../BELine_0004.h5'
+dr = sr.NSIDReader(data_path)
+# import inspect
+# from pprint import pprint
+# pprint(inspect.getmembers(dr))
+dataset_sid = dr.read()[0]
+fig = dataset_sid.plot();
+
+h5_path = '../delete_me.hdf5'
+with h5py.File(h5_path, mode='r') as h5_file:
+    h5_main = usid.hdf_utils.get_all_main(h5_file)[-1]
+    # usid.plot_utils.use_nice_plot_params()
+    h5_main.visualize()
+
+
+
+# Network drive must be mounted locally for path to be available
+# Otherwise, use data directory from this project
+network_path = Path("/Volumes/public/David Curie/Data/")
+if network_path.exists():
+    data_dir = network_path
+else:
+    data_dir = "../data"
+spe_files = list(Path.glob(data_dir, "hBN-WashU/*.SPE"))
+for i, spe_file in enumerate(spe_files):
+    print(i, spe_file.name)
 
 # Pick file from above (0-indexed)
 
-file = files[5]
-spe_object = SPE(file)
-spe_object
+spe_file = spe_files[6]
+spe_object = SpeFile(spe_file)
+# spe_object = SPE(spe_file)
+print(spe_object)
+
+# +
+# Look for position data in accompanying WIP file
+wip_file = f"{spe_object.slug}.WIP"
+wip_object = Witec(wip_file)
+
+print(witec.utils.assemble_metadata(spe_object.slug))
 
 # +
 # Look for position data in another file
