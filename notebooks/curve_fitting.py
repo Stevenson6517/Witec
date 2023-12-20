55b57fe9c457b3fbacf5dae20f75ba48e120303a
David Curie <david.a.curie@vanderbilt.edu>
2023-12-20 11:25:13 +0000

    Make curve fitting notebook use lmfit
    Use lmfit models instead of using scipy.optimize.curve_fit on hand-made
    Lorentz and Voigt models. Fitting a spectrum with multiple peaks is now
    easier to extend and manage for individual lineshapes without needing to
    build custom loops.
    
    The model in this commit is tailored specifically for one dataset
    (hBN-10-CH4_loc-TL_X07_...532nm_..._obj-100x) and may not be applicable
    to a general-purpose fit.

--- notebooks/curve_fitting.py
+++ notebooks/curve_fitting.py
@@ -6,13 +6,15 @@ from pprint import pprint
 import sys
 
 import h5py
+from lmfit.models import (
+    LorentzianModel,
+    ConstantModel,
+    VoigtModel,
+    SplitLorentzianModel,
+)
 import matplotlib.pyplot as plt
 from mpl_toolkits import axes_grid1
 import numpy as np
-import pandas as pd
-from scipy.optimize import curve_fit
-from scipy.signal import find_peaks, savgol_filter
-from scipy.special import wofz
 from tqdm import tqdm
 
 
@@ -27,94 +29,17 @@ def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
     return im.axes.figure.colorbar(im, cax=cax, **kwargs)
 
 
-# http://emilygraceripka.com/blog/16
-# https://stackoverflow.com/questions/61657107/fitting-multiple-lorentzians-to-brillouin-spectrum-using-scipy-in-python-3
-def lorentzian(x, I, x0, gamma):
-    """Return a single Lorentz curve
-    L(x) = I[ γ^2 / (x - x0)^2 + γ^2]
-    """
-    return I * gamma**2 / ((x - x0) ** 2 + gamma**2)
-
-
-def multi_lorentz(x, *params):
-    """Return a sum of multiple Lorentz curves.
-    The number of Lorentz curves is determined by the number of parameters,
-    which are expected to be passed in multiples of 3.
-    """
-    assert not (len(params) % 3)
-    return sum([lorentzian(x, *params[i : i + 3]) for i in range(0, len(params), 3)])
-
-
-def gaussian(x, A, x0, sigma):
-    """Return a single Gaussian curve
-    G(x) = A[ 1/(σ * sqrt(2π)) * (exp(-(x-x0)^2) / 2σ^2 )
-    """
-    return (
-        A
-        * (1 / sigma * (np.sqrt(2 * np.pi)))
-        * (np.exp(-((x - x0) ** 2 / (2 * (sigma**2)))))
-    )
-
-
-def multi_gauss(x, *params):
-    """Return a sum of multiple Gaussian curves.
-    The number of Gaussian curves is determined by the number of parameters,
-    which are expected to be passed in multiples of 3.
-    """
-    assert not (len(params) % 3)
-    return sum([guassian(x, *params[i : i + 3]) for i in range(0, len(params), 3)])
-
-
-# https://www.digitaldesignjournal.com/python-voigt-profile/
-def voigt_profile(x, amplitude, center, sigma, gamma):
-    z = ((x - center) + 1j * gamma) / (sigma * np.sqrt(2))
-    v = amplitude * wofz(z).real / (sigma * np.sqrt(2 * np.pi))
-    return v
-
-
-def multi_voigt(x, *params):
-    """Return a sum of multiple Voigt profiles.
-    The number of Voigt profiles is determined by the number of parameters,
-    which are expected to be passed in multiples of 4.
-    """
-    assert not (len(params) % 4)
-    return sum([voigt_profile(x, *params[i : i + 4]) for i in range(0, len(params), 4)])
-
-
-# # https://stackoverflow.com/a/71227338
-# # Modified for Lorentzian
-# def lorentzian(x, *args):
-#     """Composition function of arbitrary number of Lorentzian lineshapes.
-#        The number of Lorentzian profiles is determined by the number of arguments.
-#        Arguments are expected to appear in multiples of 3 (amp, cen, wid).
-#     """
-#     x = x.reshape(-1, 1)
-#     amp = np.array(args[0::3]).reshape(1, -1)
-#     cen = np.array(args[1::3]).reshape(1, -1)
-#     wid = np.array(args[2::3]).reshape(1, -1)
-#     return np.sum(amp * wid**2 / ((x-cen)**2 + wid**2), axis=1)
-
-# def gaussian(x, *args):
-#     """Composition function of arbitrary number of Gaussian lineshapes.
-#        The number of Guassian profiles is determined by the number of arguments.
-#        Arguments are expected to appear in multiples of 3 (amp, cen, sig).
-#     """
-#     x = x.reshape(-1, 1)
-#     amp = np.array(args[0::3]).reshape(1, -1)
-#     cen = np.array(args[1::3]).reshape(1, -1)
-#     sig = np.array(args[2::3]).reshape(1, -1)
-#     return np.sum(amp * (1/sig*(np.sqrt(2*np.pi)))*(np.exp(-((x-cen)**2 / (2 * (sig**2) )))), axis=1)
-
-# def voigt(x, *args):
-#     """Composition function of arbitrary number of Voigt lineshapes.
-#        The number of Voigt profiles is determined by the number of arguments.
-#        Arguments are expected to appear in multiples of 6 (L_amp, L_cen, L_wid, G_amp, G_cen, G_sig).
-#     """
-#     x = x.reshape(-1, 1)
-#     components = list(zip(*[iter(args)]*3)) # bundles of [amp, cen, wid]
-#     L_args = [arg for bundle in components[0::2] for arg in bundle]  # Pick out and unpack even bundles
-#     G_args = [arg for bundle in components[1::2] for arg in bundle]  # Pick out and unpack odd bunldes
-#     return (lorentzian(x, *L_args) + gaussian(x, *G_args))
+# https://stackoverflow.com/a/57295298
+def add_lorentz_peak(prefix, center, amplitude=1e3, sigma=0.1):
+    """Automate lmfit model creation for a Lorentzian peak."""
+    peak = SplitLorentzianModel(prefix=prefix)
+    pars = peak.make_params()
+    pars[prefix + "center"].set(center, min=530, max=570)
+    pars[prefix + "amplitude"].set(amplitude, min=0, max=6e5)
+    pars[prefix + "sigma"].set(sigma, min=0)
+    pars[prefix + "sigma_r"].set(sigma, min=0)
+    # pars[prefix + "gamma"].set(sigma, vary=True, min=0)
+    return peak, pars
 
 
 # This project saves data in a network drive, available only from a Vanderbilt
@@ -154,6 +79,12 @@ for i, file in enumerate(spe_files):
 h5_path = list(spe_files)[1]
 print(h5_path.name)
 
+# ## Generate best fits
+#
+# > If you've already run a best fit analysis and stored the results in an
+# .hdf5 file, you can skip this section and load the results into memory by
+# skipping to the "Read dataset from file" section.
+
 # Extract data into python variables
 with h5py.File(h5_path, mode="r") as h5_file:
     # Extract datasets from container (visible in memory as long as file is open)
@@ -167,202 +98,194 @@ with h5py.File(h5_path, mode="r") as h5_file:
     spectroscopic_dataset = temporary_spectroscopic_dataset[()].copy()
 
 # +
-index = 2950
-spectrum = main_dataset[index]
-spectrum_corrected = main_dataset[index] - np.min(main_dataset[index])
-# background = main_dataset[0]
-# corrected = spectrum - 0.9*background
-wavelengths = spectroscopic_dataset[0]
-ev = 1240 / wavelengths
+index = len(main_dataset) // 2
+spectrum = main_dataset[index][100:-40]
+# spectrum_corrected = main_dataset[index] - np.min(main_dataset[index])
+wavelengths = spectroscopic_dataset[0][100:-40]
 
-# lorentz
-# guesses = [
-#     # amp, cen, wid
-#     *[8e3, 532, 0.5],
-#     *[4e3, 536, 0.4],
-#     *[10e3, 545, 2],
-#     *[10e3, 546, 0.4],
-#     *[15e3, 550, 1],
-#     *[2e3, 556, 1],
-#     *[2e2, 560, 1],
-#     *[8e2, 563, 1],
-# ]
+# Make a model to fit the spectra
 
-# lower_bounds = [
-#     # amp, cen, wid
-#     *[0, 531.5, 0.01],
-#     *[0, 535, 0.01],
-#     *[0, 544, 0.01],
-#     *[0, 545, 0.01],
-#     *[0, 549, 0.1],
-#     *[0, 554, 0.01],
-#     *[0, 559, 0.01],
-#     *[0, 562, 0.01],
-# ]
+model = ConstantModel(prefix="bkg_")
+params = model.make_params(c=500)
+params["bkg_c"].set(550, vary=False)
 
-# upper_bounds = [
-#     # amp, cen, wid
-#     *[np.inf, 532.5, 1],
-#     *[np.inf, 537, 1],
-#     *[np.inf, 546, 5],
-#     *[np.inf, 547, 1],
-#     *[np.inf, 551, 2],
-#     *[np.inf, 557, 5],
-#     *[np.inf, 561, 5],
-#     *[np.inf, 564, 5],
-# ]
+L_shoulder = SplitLorentzianModel(prefix="sL0_")
+params.update(
+    L_shoulder.make_params(
+        center={"value": 536, "min": 535.5, "max": 536.5},
+        sigma={"value": 0.1, "min": 0, "max": 1},
+        sigma_r={"value": 2.5, "vary": False},
+        amplitude={"value": 6.3e3, "min": 0},
+    )
+)
+residue = SplitLorentzianModel(prefix="sL1_")
+params.update(
+    residue.make_params(
+        center={"value": 544.6, "min": 544, "max": 545},
+        sigma={"value": 5.2, "min": 5.0},
+        sigma_r={"value": 1.64, "min": 1.5, "max": 2.5},
+        amplitude={"value": 102e3, "min": 0},
+    )
+)
+zpl = LorentzianModel(prefix="L2_")
+params.update(
+    zpl.make_params(
+        center={"value": 546.35, "min": 546.0, "max": 546.6},
+        sigma={"value": 0.8, "min": 0},
+        amplitude={"value": 14e3, "min": 0},
+    )
+)
+psb1 = VoigtModel(prefix="v3_")
+params.update(
+    psb1.make_params(
+        center={"value": 549.6},
+        sigma={"value": 0.31, "min": 0},
+        # gamma={"value": 0.01, "vary": False, "min": 0},
+        amplitude={"value": 2.5e3, "min": 0},
+    )
+)
+psb2 = SplitLorentzianModel(prefix="sL4_")
+params.update(
+    psb2.make_params(
+        center={"value": 555.8, "min": 555.5, "max": 556},
+        sigma={"value": 1.1, "min": 1, "max": 1.5},
+        sigma_r={"value": 1.2, "min": 1, "max": 1.5},
+        amplitude={"value": 6.8e3, "min": 0},
+    )
+)
+psb3 = VoigtModel(prefix="v5_")
+params.update(
+    psb3.make_params(
+        center={"value": 560, "min": 559, "max": 561},
+        sigma={"value": 0.2, "min": 0},
+        # gamma={"value": 0.01, "vary": False, "min": 0},
+        amplitude={"value": 1.0e2, "min": 0},
+    )
+)
+psb4 = VoigtModel(prefix="v6_")
+params.update(
+    psb4.make_params(
+        center={"value": 563.5, "min": 563.1, "max": 564},
+        sigma={"value": 0.64, "min": 0.3, "max": 0.9},
+        # gamma={"value": 1.6, "vary": False, "min": 0, max=2},
+        amplitude={"value": 1.8e3, "min": 0},
+    )
+)
 
-# voigt
-guesses = [
-    # amp, cen, wid
-    *[8e3, 532, 0.5, 0.5],
-    *[4e3, 536, 0.4, 0.5],
-    *[10e3, 545, 2, 0.5],
-    *[10e3, 546, 0.4, 0.5],
-    *[15e3, 550, 1, 0.5],
-    *[2e3, 556, 1, 0.5],
-    *[2e2, 560, 1, 0.5],
-    *[8e2, 563, 1, 0.5],
-]
+model += L_shoulder + residue + zpl + psb1 + psb2 + psb3 + psb4
 
-lower_bounds = [
-    # amp, cen, wid
-    *[0, 531.5, 0.01, 0.1],
-    *[0, 535, 0.01, 0.1],
-    *[0, 544, 0.01, 0.1],
-    *[0, 545, 0.01, 0.1],
-    *[0, 549, 0.1, 0.1],
-    *[0, 554, 0.01, 0.1],
-    *[0, 559, 0.01, 0.1],
-    *[0, 562, 0.01, 0.1],
-]
-upper_bounds = [
-    # amp, cen, wid
-    *[np.inf, 532.5, 1, 1],
-    *[np.inf, 537, 1, 1],
-    *[np.inf, 546, 5, 1],
-    *[np.inf, 547, 1, 1],
-    *[np.inf, 551, 2, 1],
-    *[np.inf, 557, 5, 1],
-    *[np.inf, 561, 5, 1],
-    *[np.inf, 564, 5, 1],
-]
-
-bounds = (lower_bounds, upper_bounds)
-
-# Fit a single spectrum to a curve
-popt, pcov = curve_fit(multi_voigt, wavelengths, spectrum_corrected, p0=guesses)
-# popt, pcov = curve_fit(multi_voigt, wavelengths, spectrum_corrected, p0=guesses, bounds=bounds)
-err = np.sqrt(np.diag(pcov))
-# y_fit = lorentzian(wavelengths, *popt)
-y_fit = multi_voigt(wavelengths, *popt)
-print(*popt)
-components = list(zip(*[iter(popt)] * 4))  # list of [amp, cen, wid] fit parameters
-components_err = list(zip(*[iter(err)] * 4))  # list of [amp, cen, wid] fit parameters
-pprint(pd.DataFrame(components))
+init = model.eval(params, x=wavelengths)
+result = model.fit(spectrum, params, x=wavelengths)
+comps = result.eval_components()
+print(result.fit_report(min_correl=0.5))
 
 with plt.style.context("default"):
     fig, ax = plt.subplots()
     ax.set_xlabel("Wavelength (nm)")
     ax.set_ylabel("Intensity")
-    ax.plot(wavelengths, spectrum_corrected, label="corrected")
-    ax.plot(wavelengths, y_fit, label="fit", alpha=0.85)
-    for component in components:
-        ax.plot(
-            wavelengths,
-            multi_voigt(wavelengths, *component),
-            linewidth=0.75,
-            label=round(component[1]),
-            alpha=0.5,
-        )
+    ax.scatter(wavelengths, spectrum, s=1, color="gray", label="spectrum")
+    ax.plot(wavelengths, result.best_fit, label="best fit", alpha=0.85)
+    ax.plot(wavelengths, init, "-", label="initial fit", alpha=0.85)
+    for name, comp in comps.items():
+        ax.plot(wavelengths, comp, "--", linewidth=0.75, label=name, alpha=0.5)
     plt.legend()
     plt.show()
 # -
 # If the above fit looks good, run it on the entire dataset below.
 
-model = {
-    "Equation": lorentzian,
-    "Initial Guess": guesses,
-    "Bounds": bounds,
-    "Notes": "Spectra were first corrected according to 'spectra = spectra - np.min(spectra)'",
-}
-print(help(lorentzian))
-
 # +
-fits = []
-errors = []
+values = {}
+stats = []
 for spectrum in tqdm(main_dataset):
-    spectrum = spectrum - np.min(spectrum)
-    popt, pcov = curve_fit(lorentzian, wavelengths, spectrum, p0=guesses, bounds=bounds)
-    err = np.sqrt(np.diag(pcov))
-    y_fit = lorentzian(wavelengths, *popt)
-    components = list(zip(*[iter(popt)] * 3))  # list of [amp, cen, wid] fit parameters
-    components_err = list(
-        zip(*[iter(err)] * 3)
-    )  # list of [amp, cen, wid] fit parameters
-    fits.append(components)
-    errors.append(components_err)
+    spectrum = spectrum[100:-40]
+    result = model.fit(spectrum, params, x=wavelengths)
+    try:
+        result_dict = result.uvars
+    except AttributeError:
+        result_dict = result.best_values
+    for key, value in result_dict.items():
+        if values.get(key):
+            existing_value = values[key]
+        else:
+            existing_value = list()
+        try:
+            existing_value.append((value.n, value.s))
+        except AttributeError:
+            existing_value.append((value, 0))
+        values[key] = existing_value
+    stats.append(result.dumps())
 
-# convert lists to arrays for better storage and access
-fits = np.asarray(fits)
-errors = np.asarray(errors)
+# Format all data elements as array instead of list
+for key, value in values.items():
+    values[key] = np.asarray(value)
+stats = np.asarray(stats)
 # -
 
-print("fits:", fits.shape)
-print("errors:", errors.shape)
+# Inspect values to see if they have been populated correctly
+pprint(values)
+
+# ## Write dataset to file
+#
+# If the above values dictionary is populated the way you want, run the next
+# cell to save the results to the file.
+#
+# WARNING: Including the `stats` dataset will increase the .hdf5 file by
+# approximately a factor of `n`, where `n` is the number of individual models
+# in the composite model for your best fit.
 
 with h5py.File(h5_path, mode="r+") as h5_file:
-    # Write datasets to h5 file for future access
+    dt = h5py.special_dtype(vlen=str)
+    data_stats = np.asarray(stats, dtype=dt)
     h5_file.create_dataset(
-        "Measurement_000/Analysis/Lorentz_fits/model", data=str(model)
+        "Measurement_000/Analysis/Custom_fits/best_fit/stats", data=data_stats
     )
-    h5_file.create_dataset(
-        "Measurement_000/Analysis/Lorentz_fits/optimized_params", data=fits
-    )
-    h5_file.create_dataset(
-        "Measurement_000/Analysis/Lorentz_fits/optimized_errors", data=errors
-    )
+    for key, best_fit in values.items():
+        h5_file.create_dataset(
+            f"Measurement_000/Analysis/Custom_fits/best_fit/{key}",
+            data=best_fit.astype(float),
+        )
 
+# ## Read dataset from file
+
+# Quick sanity check to see that contents in h5 are retrievable
 with h5py.File(h5_path, mode="r") as h5_file:
-    data = h5_file["Measurement_000/Analysis/Lorentz_fits/model"]
-    print(data[()])
+    best_fits = h5_file["Measurement_000/Analysis/Custom_fits/best_fit"]
+    memory_results = {}
+    for label, value in best_fits.items():
+        if "bkg" in label or "stats" in label:
+            continue
+        memory_results[label] = value[()]
+    my_results = memory_results.copy()
+
+# Examine sample output
+my_number, my_uncertainty = my_results["L2_amplitude"].T
+pprint("Total number of keys:", len(my_results.keys()))
+print("Size of complete key:", my_number.shape)  # Should be (num_pixels,)
+keys_to_delete = []
+# Remove any keys with missing data because they prevent the next resizing
+# efforts to fail. Missing data occurs when a fit fails to optimize at
+# a particular pixel.
+for key, value in my_results.items():
+    if len(value) != len(my_number):
+        keys_to_delete.append(key)
+print("Dropping the following incomplete keys:", keys_to_delete)
+for key in keys_to_delete:
+    del my_results[key]
+
+# ## View components as maps
 
 # +
+# Extract physical coordinates for resizing
 rows = np.unique(position_dataset[:, 0])  # x coordinates
 cols = np.unique(position_dataset[:, 1])  # y coordinates
 position_values = np.reshape(position_dataset, (len(rows), len(cols), -1))
-fits = np.asarray(fits)
 
-intensity_values = np.sum(
-    main_dataset, axis=1
-)  # Sum intensity across all pixels in each spectrum
-intensity_map = np.reshape(
-    intensity_values, (len(rows), len(cols), -1)
-)  # shape (x, y, I) where I is Intensity
+# Sum intensity across all wavelengths in each spectrum
+intensity_values = np.sum(main_dataset, axis=1)
+# Reshape to (x, y, I) where I is Intensity
+intensity_map = np.reshape(intensity_values, (len(rows), len(cols), -1))
 
-for peak in range(0, 8):
-    amp, cen, wid = fits[:, peak, :].T
-    amp_map = np.reshape(amp, (len(rows), len(cols), -1))
-    cen_map = np.reshape(cen, (len(rows), len(cols), -1))
-    wid_map = np.reshape(wid, (len(rows), len(cols), -1))
-    with plt.style.context("default"):
-        fig, axs = plt.subplots(1, 3, figsize=(12, 3.5))
-        fig.suptitle(f"{round(cen[0])} nm peak")
-        axs[0].set_title("Amplitude")
-        axs[0].set_axis_off()
-        amp_img = axs[0].imshow(amp_map, cmap="gist_stern")
-        add_colorbar(amp_img)
-        axs[1].set_title("Center wavelength")
-        axs[1].set_axis_off()
-        cen_img = axs[1].imshow(cen_map, cmap="gist_stern")
-        add_colorbar(cen_img)
-        axs[2].set_title("Width")
-        axs[2].set_axis_off()
-        wid_img = axs[2].imshow(wid_map, cmap="gist_stern")
-        add_colorbar(wid_img)
-        plt.tight_layout()
-        plt.show()
-
+# Plot the intensity value at each pixel
 with plt.style.context("default"):
     fig, ax = plt.subplots()
     ax.set_title(f"{h5_path.stem}\n")
@@ -374,4 +297,20 @@ with plt.style.context("default"):
     # print(f"Figure saved to {output}")
     plt.show()
 
-# -
+# Do the above for each component in the best fits
+COMPONENTS_SIZE = len(my_results)
+with plt.style.context("default"):
+    NCOLS = 3
+    NROWS = COMPONENTS_SIZE // ncols + (COMPONENTS_SIZE % NCOLS > 0)
+    fig = plt.figure(figsize=(NCOLS * 3, NROWS * 3))
+    fig.suptitle(f"{h5_path.stem}")
+    for i, component in enumerate(my_results):
+        ax = plt.subplot(NROWS, NCOLS, i + 1)
+        my_value, my_unc = my_results[component].T
+        my_value = np.reshape(my_value, (len(rows), len(cols), -1))
+        my_unc = np.reshape(my_unc, (len(rows), len(cols), -1))
+        ax.set_title(f"{component}")
+        ax.set_axis_off()
+        add_colorbar(ax.imshow(my_value, cmap="gist_stern"))
+    plt.tight_layout()
+    plt.show()
